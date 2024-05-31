c0dedf7b30411a9d7a2832f9f2eed2f6eb3e2e65
Hüsam <husamahmud@gmail.com>
2024-05-31 17:29:15 +0300

    [1] [Time Beats: 53.81%] [Memory Beats: 39.82%] - LeetPush

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
         map = {}
-        # dshaklhf
+
         for i in range(n):
             dif = target - nums[i]
             if dif in map:
